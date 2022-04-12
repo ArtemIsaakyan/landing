1d59ae019709aad3fb72c115253d9b1dae6b8c59
ArtemIsaakyan <artemisaakian.school@gmail.com>
2022-04-13 01:06:44 +0300

    second commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
+import Swiper, { Navigation, Autoplay } from 'swiper';
 import './components/nav';
+
+// const swiper = new Swiper('.swiper', {
+//   speed: 400,
+//   slidesPerView: 1,
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
